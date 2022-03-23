77ac7bab987ca4d0867b09b6fa961a0c6482aefe
Fabian Darga <kontakt@fabiandarga.de>
2022-03-23 13:51:15 +0100

    Fix photo field name

--- pohtographer/components/AddPhoto.jsx
+++ pohtographer/components/AddPhoto.jsx
@@ -4,36 +4,39 @@
 
   const BACKEND_URL =
   process.env.NEXT_PUBLIC_BACKEND_URL; /* || 'http://localhost:4000' */
-const RECORDS_PATH = BACKEND_URL ;
+const RECORDS_PATH = BACKEND_URL;
+const API_PHOTO_NAME = "uploaded_file"
 
-  const [newFile, setNewFile] = useState({
-    photo: "",
-  });
-  const [imageSrc, setImageSrc] = useState("");
+  const [newFile, setNewFile] = useState({ photo: "" });
+
+  const [imageSrc, setImageSrc] = useState(""); // Bild als Daten (String)
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append("uploaded_file", newFile.uploaded_file);
+    formData.append(API_PHOTO_NAME, newFile.photo);
     //console.log(formData)
     fetch(RECORDS_PATH + "/photos/new", {
       method: "POST",
       body: formData,
     });
   };
+
   const handlePhoto = (e) => {
     const [file] = e.target.files;
     if (file) {
       setImageSrc(URL.createObjectURL(file));
     }
-    setNewFile({ ...newFile, photo: e.target.files[0] });
+    setNewFile({ photo: file });
   };
+
   return (
     <div>
       <form onSubmit={handleSubmit} encType="multipart/form-data">
         <input
           type="file"
           accept=".png, .jpg, .jpeg"
-          name="uploaded_file"
+          name="photo"
           onChange={handlePhoto}
         />
         <br />
