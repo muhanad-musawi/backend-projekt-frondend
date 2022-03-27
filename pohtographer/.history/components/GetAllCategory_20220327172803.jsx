7bddd6185b42dd818940f64e8a5f8d83e5f292f1
muhanad-al-musawi <al-musawi-muhanad@hotmail.com>
2022-03-27 22:15:42 +0200

    Gallery Bearbeitet

---
+++ pohtographer/.history/components/GetAllCategory_20220327172803.jsx
@@ -0,0 +1,26 @@
+import styles from '../styles/GetAllCategory.module.css' 
+
+
+
+
+function GetAllCategory ({categories, handleInputChangeCategory}){
+  
+  return (
+    <div className={styles.imageCategory} >
+      {categories.map((category, index) => (
+        <div key={index}>
+          <ul className={styles.ulCategory} key={index}>
+            <li key={index}>
+              <button  
+               onClick={e=>handleInputChangeCategory(category.categorie)}>
+              {category.categorie} 
+              </button>
+            </li>
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default GetAllCategory;
